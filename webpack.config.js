def81a1909b98339c3b66a221b67b67511d2ef8b
Yoojeong Lee <yjclarelee@p4ace.com>
2021-09-02 13:35:47 +0900

    Initialize project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', 
+  output: { 
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    open: true,
+  },
+};
